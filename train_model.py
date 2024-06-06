c7a5bed56b0abcb3128a9af7f2f63dc1ae488664
saathby1122 <57776812+saathby1122@users.noreply.github.com>
2024-06-06 15:19:36 -0400

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Load and preprocess the data
+data = pd.read_csv('healthcare_data.csv')  # Make sure you have this dataset
+data = pd.get_dummies(data, drop_first=True)  # Encode categorical variables
+
+# Define features and target
+X = data.drop('outcome', axis=1)
+y = data['outcome']
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save the model
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
